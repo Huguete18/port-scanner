d7d917e71c2b7ae5876706bcc7f35d92d4f1e598
Huguete18 <serhuguet@protonmail.com>
2023-12-20 12:09:10 +0100

    Finalizado

--- portscanner.py
+++ portscanner.py
@@ -1,16 +1,32 @@
 import socket
+import time
+import concurrent.futures
 
-ip = input("Enter the ip address:")
 
-for port in range(1, 65535):
+def scan_port(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(5)
-    
     result = sock.connect_ex((ip, port))
-    
     if result == 0:
-        print("Port is open: " + str(port))
-        sock.close()
+        return f"Port is open: {port}"
     else:
-        print("Port is not open: " + str(port))
-        sock.close()
+        return f"Port is not open: {port}"
+
+
+def main():
+    ip = input("Enter the ip address: ")
+    start_time = time.time()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = {
+            executor.submit(scan_port, ip, port): port for port in range(1, 65535)
+        }
+        for future in concurrent.futures.as_completed(futures):
+            print(future.result())
+
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
+
+
+if __name__ == "__main__":
+    main()
